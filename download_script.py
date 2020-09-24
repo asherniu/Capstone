60d0d541f381e672ce676846ef0b96cc7053dfb7
Shihao Niu <asherniu@gmail.com>
2020-09-23 18:18:32 -0700

    Change data source from IEX to Yahoo

--- download_script.py
+++ download_script.py
@@ -21,17 +21,32 @@ def download(code):
         print(err)
 
 
+def download_yahoo(code):
+    """
+    Download the stock data from Yahoo API and save it as csv
+    It is better since it has higher limits
+    :param code: stock code
+    :return: null
+    """
+    try:
+        print(f"Downloading stock {code}")
+        df = web.DataReader(code, 'yahoo', start_date, end_date)
+        output = code + ".csv"
+        df.to_csv(output)
+    except Exception as err:
+        print(f"Downloading stock {code} failed")
+        print(err)
+
+
 if __name__ == '__main__':
     """Set the start and end time of historical data"""
-    start_date = datetime(2015, 6, 11)
-    end_date = datetime(2020, 6, 11)
+    start_date = datetime(2015, 11, 1)
+    end_date = datetime(2019, 11, 1)
     print(f"The start date is set to {start_date}, and the end date is set to {end_date}")
 
     """Free account can only download about 3 stocks per month."""
-    codes = ['AAPL', 'MSFT', 'AMZN']
-    # TODO: download the failed stocks next month.
-    failed_codes = ['FB', 'GOOGL']
+    codes = ['MSFT', 'AMZN','AAPL', 'FB', 'GOOGL']
     print(f"The stocks code I will be downloading are {codes}")
 
     for code in codes:
-        download(code)
+        download_yahoo(code)
